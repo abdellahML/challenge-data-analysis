44d75984151142de0c24a133b685725ecac65b89
abdellah <abdellah.ghilbzouri@gmail.com>
2020-12-04 09:35:51 +0100

    quantitative data

---
+++ Codes/test.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+import numpy as np
+from DataCleaning import open_and_manage
+from Main_abdellah import abdellah
+
+
+abdellah('~/challenge-data-analysis/quick_clean.csv')
+#left=0.255,
+#right=0.99,
